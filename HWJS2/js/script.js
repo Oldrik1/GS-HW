a11a9124a0ec37503de3d799e3c0c3d106236cfe
Oldrik1 <112575879+Oldrik1@users.noreply.github.com>
2024-03-13 16:18:27 +0200

    Add files via upload

---
+++ HWJS2/js/script.js
@@ -0,0 +1,108 @@
+//***1***
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3
+
+// const a = 1;
+
+// if (a > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+//***2***
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test', "qwerty", true
+
+// const sring = "test";
+// if (sring === "test") {
+//     console.log("tak");
+// } else {
+//     console.log("no");
+// }
+
+
+
+//***3***
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13
+
+// const num = 11;
+// if (num > 10) {
+//    const minus = num - 5;
+//     console.log(minus);
+// } else {
+//     const plus = num + 5;
+//     console.log(plus);
+// }
+
+
+
+
+//***4***
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const i = prompt("Введіть число");
+//     switch (i) {
+        
+//         case "1":
+//               console.log("Junary");
+//             break;
+                
+//         case "2":
+//             console.log("February");
+//             break;
+        
+//         case "3":
+//             console.log("March");
+//             break;
+        
+//         case "4":
+//             console.log("April");
+//             break;
+        
+//         case "5":
+//             console.log("May");
+//             break;
+        
+//         case "6":
+//             console.log("June");
+//             break;
+        
+//         case "7":
+//             console.log("July");
+//             break;
+        
+//         case "8":
+//             console.log("August");
+//             break;
+        
+//         case "9":
+//             console.log("September");
+//             break;
+        
+//         case "10":
+//             console.log("October");
+//             break;
+        
+//         case "11":
+//             console.log("November");
+//             break;
+        
+//         case "12":
+//             console.log("December");
+//             break;
+        
+//         default:
+//             console.log("No mounth");
+//     }
+
+//***5***
+// Зробіть сервіс який отримує тризначне число
+// Поверніть користувачу сумму цих чисел
+
+
+//--------------------------
