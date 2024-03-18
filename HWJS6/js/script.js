7a3a83d5801750f0aa80933db664ded6b87350f3
Oldrik1 <112575879+Oldrik1@users.noreply.github.com>
2024-03-18 13:43:07 +0200

    Add files via upload

---
+++ HWJS6/js/script.js
@@ -0,0 +1,44 @@
+//Створити телефонну книгу
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+
+// class Abonent {
+//      AbonentInfo(info){
+//          this.name = info.name;
+//          this.tel = info.tel;
+//     }
+//     name = "";
+//     tel = "";
+
+//     set addAbonent(newAbonent) {
+//         const addAbonentRow = newAbonent.split(" ");
+//         this.name = newAbonent[0];
+//         this.tel = newAbonent[1];
+//     }
+
+//     get() {
+//         return `Name Abonent: ${this.name} Tel Abonent: ${this.tel}`
+//     }
+// }
+
+// const Oleg = {
+//     name: "Oleg",
+//     tel: "+380XXXXXXX"
+// }
+
+// const Slava = {
+//     name: "Slava",
+//     tel: "+48XXXXXXX"
+// }
+
+// const Mike = {
+//     name: "Mike",
+//     tel: "+1XXXXX"
+// }
+
+// console.log(Slava);
+
+
