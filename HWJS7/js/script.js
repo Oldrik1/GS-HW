df344c72a0d0715bfbcc8b8c85ff03acb9b0cf76
Oldrik1 <112575879+Oldrik1@users.noreply.github.com>
2024-03-19 13:36:52 +0200

    Add files via upload

---
+++ HWJS7/js/script.js
@@ -0,0 +1,31 @@
+// ----1----
+// Створіть функцію-конструктор Calculator, який створює об’єкти з трьома
+// методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у
+// властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей.
+
+// function Calculator(sznacz1, sznacz2) {
+
+//     this.sznacz1 = sznacz1;
+//     this.sznacz2 = sznacz2;
+
+//     function read(){
+//          sznacz1 = prompt("Введіть число");
+//          sznacz2 = prompt("Введіть число");
+//     };
+
+//     function sum(){
+//         alert(sznacz1 + sznacz2);
+//     };
+    
+//     function mul() {
+//         alert(sznacz1 * sznacz2);
+//     };
+// }
+
+
+
+
+
